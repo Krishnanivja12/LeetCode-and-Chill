7084cbfd2be08a530b78628e7fd251a192339f03
Krishna Nivja <krishnanivja249@gmail.com>
2026-09-23 23:52:00 +0530

    Time: 576 ms (91.75%), Space: 18.4 MB (60.17%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,36 @@
 class Solution(object):
     def threeSum(self, nums):
-        answer = []
         nums.sort()
-        for i in range(len(nums) - 2):
+        result = []
+        n = len(nums)
 
-            if i > 0 and nums[i] == nums[i-1]:
+        for i in range(n - 2):
+            if nums[i] > 0:
+                break
+
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
+
             left = i + 1
-            right = len(nums) - 1
+            right = n - 1
 
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
+                total_sum = nums[i] + nums[left] + nums[right]
 
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
-                else:
-                    answer.append([nums[i], nums[left], nums[right]])
+                if total_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
 
-                    left += 1
-                    right -= 1
-
-                    while left < right and  nums[left] == nums[left - 1]:
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    
-                    while right > left and nums[right] == nums[right + 1]:
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
 
-        return answer
+                    left += 1
+                    right -= 1
 
+                elif total_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
 
-        
+        return result
